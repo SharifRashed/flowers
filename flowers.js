9b53c8223dcf99cdb9c05555596e9c7138b9dc03
sharifrashed <sharif.rashed26@gmail.com>
2021-11-18 13:54:42 -0600

    added flowers array

--- flowers.js
+++ flowers.js
@@ -1,22 +1,27 @@
 const flowers = [
+    {
+        id: 1,
+        color: "white",
+        species: "rose",
+        price: 0.90,
+
+    },
+
+    {
+        id: 2,
+        color: "red",
+        species: "Tulip",
+        price: 1.10,
+
+    }
+
 
 ]
 
 const addFlower = (flowerObject) => {
-    /*
-        This function should add an `id` property
-        to the object that was sent to the function.
-        The value of the `id` property should be 1
-        greater than the current maximum `id` in the
-        array.
-
-        Once the `id` property has been added to the,
-        object, the object should then be put into the
-        `flowers` array.
-    */
-
 
 }
+
 
 
 const findExpensiveFlowers = () => {
@@ -29,7 +34,12 @@ const findExpensiveFlowers = () => {
         greater than or equal to 1.00, it should be
         added to the `expensiveFlowers` array.
     */
-
+    for (const flower of flowers) {
+        if (toy.id === toyToFind) {
+            toy.price = toy.price + 0.5
+            console.log(`${toy.name} by ${toy.maker} costs $${toy.price}`)
+        }
+    }
 
 
 
